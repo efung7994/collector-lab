930fd0d98c1a1886a1b4e0a0eea1dd864ed07574
Eugene <eugenefung79@gmail.com>
2023-06-01 13:20:00 -0400

    finish croc-collector part 7 auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('crocs/', views.croc_index, name='croc-index'),
   path('crocs/<int:croc_id>/', views.croc_detail, name='croc-detail'),
@@ -15,5 +15,6 @@ urlpatterns = [
   path('toys/', views.ToyList.as_view(), name='toy-index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete'),
-  path('crocs/<int:croc_id>/assoc-toy/<int:toy_id>/', views.assoc_toy, name='assoc-toy')
+  path('crocs/<int:croc_id>/assoc-toy/<int:toy_id>/', views.assoc_toy, name='assoc-toy'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Croc, Toy
 from .forms import FeedingForm
 
@@ -8,35 +13,42 @@ from .forms import FeedingForm
 from django.http import HttpResponse
 
 # Define the home view
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def croc_index(request):
-  crocs = Croc.objects.all()
+  crocs = Croc.objects.filter(user=request.user)
   return render(request, 'crocs/index.html', { 'crocs': crocs })
 
+@login_required
 def croc_detail(request, croc_id):
   croc = Croc.objects.get(id=croc_id)
   toys_croc_doesnt_have = Toy.objects.exclude(id__in = croc.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'crocs/detail.html', { 'croc': croc, 'feeding_form': feeding_form, 'toys': toys_croc_doesnt_have })
 
-class CrocCreate(CreateView):
+class CrocCreate(LoginRequiredMixin, CreateView):
   model = Croc
   fields = ['name', 'breed', 'description', 'age']
   success_url = '/crocs/'
 
-class CrocUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class CrocUpdate(LoginRequiredMixin, UpdateView):
   model = Croc
   fields = ['breed', 'description', 'age']
 
-class CrocDelete(DeleteView):
+class CrocDelete(LoginRequiredMixin, DeleteView):
   model = Croc
   success_url = '/crocs/'
 
+@login_required
 def add_feeding(request, croc_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -45,24 +57,39 @@ def add_feeding(request, croc_id):
     new_feeding.save()
   return redirect('croc-detail', croc_id=croc_id)
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys/'
 
+@login_required
 def assoc_toy(request, croc_id, toy_id):
   Croc.objects.get(id=croc_id).toys.add(toy_id)
   return redirect('croc-detail', croc_id=croc_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('croc-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
